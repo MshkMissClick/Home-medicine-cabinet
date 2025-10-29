c38bffcbf60f30a9dbefdd7cfb6cce751fa20fd3
Maria Kharkova <harkovam50@gmail.com>
2025-10-29 20:18:17 +0300

    frontend sheme

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>💊 MedCabinet</h1>
+            <p>Frontend is working!</p>
+        </div>
+    )
+}
+
+export default App
